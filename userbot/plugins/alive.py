544b7ee0b4fc9cbf3523f82406c5c81a273d5fbd
spyzz45 <66818186+spyzz45@users.noreply.github.com>
2020-07-08 14:26:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,10 +17,10 @@ ALIVE_caption += "`👉🏻DATABASE STATUS👈🏻:` **Functional**\n\n"
 ALIVE_caption += "**👹Current Branch👹** : `master`\n\n"
 ALIVE_caption += "**🍁Friday OS🍁** : `3.14`\n\n"
 ALIVE_caption += "**🚵Current Sat🚵** : `@whitedevil-2.25`\n\n"
-ALIVE_caption += f"**🍁My Boss🍁** : {DEFAULTUSER} \n\n"
+ALIVE_caption += f"**🍁My right full owner🍁** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**🌿Heroku Database🌿** : `AWS - Working Properly`\n\n"
 ALIVE_caption += "**✌️License✌️** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n\n"
-ALIVE_caption += "Copyright By [spyzz45@Github](GitHub.com/Spyzz45)\n\n"
+ALIVE_caption += "Copyright By [@Github](GitHub.com)\n\n"
 ALIVE_caption += "[Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
